c4b534a9e6a4ed6877a3bb46a12925fdf8e8d1a8
clivejj <cjohnston1@stuy.edu>
2018-05-27 20:37:28 -0400

    updates

--- js.js
+++ js.js
@@ -1,33 +1,51 @@
 var svg = document.getElementById("svg");
 var clear = document.getElementById("clear");
 var start = document.getElementById("start");
+var textbox = document.getElementById("textbox");
+var submit = document.getElementById("submit");
 var NS = "http://www.w3.org/2000/svg"
 var elements = [];
 var requestId;
 var move = false;
+var lastCircleData;
+var lastCircleDrawn;
+var newCircleAllowed = true;
 
 //svg.setAttribute("width", window.innerWidth);
 //svg.setAttribute("height", window.innerHeight);
 
+textbox.style.visibility = "hidden";
+submit.style.visibility = "hidden";
+
 
 var newCircle = function(e) {
-    console.log("new circle");
-    var circle = {
+    if (!(newCircleAllowed)) {
+	return;
+    }
+       lastCircleData = {
 	x : e.offsetX,
 	y : e.offsetY,
 	vx : 0,
 	vy : 0,
 	ax : 0,
 	ay : 0,
-	q : 1,
+	q : 5,
     };
-    drawCircle(circle);
-    elements.push(circle);
+    lastCircleDrawn = drawCircle(lastCircleData);
+    elements.push(lastCircleData);
+    textbox.style.visibility = "visible";
+    submit.style.visibility = "visible";
+    submit.disabled = true;
+    console.log("new circle");
+    newCircleAllowed = false;
 }
 
 var updateCircles = function() {
     for (var i = 0; i < elements.length; i++) {
-	circle = elements[i];
+	var circle = elements[i];
+	if (circle == lastCircleData) {
+	    return;
+	}
 	//console.log("before updating...");
 	//console.log(circle.x);
 	//console.log(circle.y);
@@ -52,20 +70,35 @@ var drawCircle = function(circle) {
     console.log(circle.y);
     c.setAttribute("cx", circle.x);
     c.setAttribute("cy", circle.y);
-    c.setAttribute("r", 20);
-    c.setAttribute("fill", "lightsteelblue");
+    c.setAttribute("r", Math.sqrt(Math.abs(circle.q)) * 13);
+    if (circle.q > 0) {
+	console.log("positive");
+	c.setAttribute("fill", "green");
+    }
+    else {
+	c.setAttribute("fill", "red");
+    }
     c.setAttribute("stroke","black");
     svg.appendChild(c);
+    return c;
 }
 
 var animate = function(){
     console.log(elements);
     console.log("animating");
-    svg.innerHTML = "";
+    var i = 0;
+    console.log(svg.children);
+    var max = svg.childElementCount - 1;
+    while (i < max) {
+	svg.removeChild(svg.children[0]);
+	i++;
+    }
     updateCircles();
     for (var i = 0; i < elements.length; i++) {
-	circle = elements[i];
-	drawCircle(circle);
+	var circle = elements[i];
+	if (circle != lastCircleData) {
+	    drawCircle(circle);
+	}
     }
 }
 
@@ -76,6 +109,7 @@ var end = function(e) {
     elements = [];
     move = false;
     start.innerHTML = "start";
+    newCircleAllowed = true;
 }
 
 var action = function(e) {
@@ -90,8 +124,34 @@ var action = function(e) {
 	move = false;
     }
 }
+
+var updateCharge = function(e) {
+    submit.disabled = true;
+    input = textbox.value;
+    console.log("updating");
+    console.log(input);
+    if (input >= -100 && input <= 100 && input != "") {
+	submit.disabled = false;
+	svg.removeChild(lastCircleDrawn);
+	lastCircleData.q = input / 10
+	lastCircleDrawn = drawCircle(lastCircleData);
+    }
+}
 	
+
+var assignCharge = function(e) {
+    newCircleAllowed = true;
+    lastCircleData = null;
+    lastCircleData = null;
+    textbox.style.visibility = "hidden";
+    submit.style.visibility = "hidden";
+    
+    
+    
+}
     
 svg.addEventListener("click", newCircle);
 clear.addEventListener("click", end);
 start.addEventListener("click", action);
+submit.addEventListener("click", assignCharge);
+textbox.addEventListener("input", updateCharge);
