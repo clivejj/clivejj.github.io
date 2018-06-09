79413caf0ea743dac8feb863ab101d781fd7c84a
clivejj <cjohnston1@stuy.edu>
2018-06-09 07:52:04 -0400

    fix colors

--- js.js
+++ js.js
@@ -15,6 +15,7 @@ var newCircleAllowed = true;
 //svg.setAttribute("height", window.innerHeight);
 
 textbox.style.visibility = "hidden";
+textbox.value = 50;
 submit.style.visibility = "hidden";
 
 
@@ -35,8 +36,8 @@ var newCircle = function(e) {
     elements.push(lastCircleData);
     textbox.style.visibility = "visible";
     submit.style.visibility = "visible";
-    submit.disabled = true;
-    console.log("new circle");
+    submit.disabled = false;
+    //console.log("new circle");
     newCircleAllowed = false;
 }
 
@@ -55,28 +56,51 @@ var updateCircles = function() {
 	circle.vy += circle.ay;
 	//ax = calcAx(circle);
 	//ay = calcAy(circle);
-	circle.ax = .01;
-	circle.ay = -.01;
+	var a = calcA(circle);
+	circle.ax = a[0];
+	circle.ay = a[1];
 	//console.log("after updating...")
 	//console.log(circle.cx);
 	//console.log(circle.cy);
     };
 }
 
+var calcA = function(circle) {
+    var sumAX = 0;
+    var sumAY = 0;
+    for (var i = 0; i < elements.length; i++) {
+	var otherCircle = elements[i];
+	if ((otherCircle != lastCircleData) && (otherCircle != circle)) {
+	    var d2 = (Math.pow((circle.x - otherCircle.x), 2) + Math.pow((circle.y - otherCircle.y), 2));
+	    var angle = Math.atan((circle.y - otherCircle.y) / (circle.x - otherCircle.x));
+	    var q = otherCircle.q
+	    sumAX += q * Math.cos(angle) / d2
+	    sumAY += q * Math.sin(angle) / d2
+	}
+    }
+    var result = [sumAX * circle.q, sumAY * circle.q];
+    console.log(result);
+    return(result);
+    //return [.01, .01];
+}
+
 var drawCircle = function(circle) {
-    console.log("drawing circle");
+    //console.log("drawing circle");
     var c = document.createElementNS(NS, "circle");
-    console.log(circle.x);
-    console.log(circle.y);
+    //console.log(circle.x);
+    //console.log(circle.y);
     c.setAttribute("cx", circle.x);
     c.setAttribute("cy", circle.y);
-    c.setAttribute("r", Math.sqrt(Math.abs(circle.q)) * 13);
+    //c.setAttribute("r", Math.sqrt(Math.abs(circle.q)) * 13);
+    c.setAttribute("r", 15);
     if (circle.q > 0) {
-	console.log("positive");
-	c.setAttribute("fill", "green");
+	var color = "rgba(0,255,0," + (circle.q/10) + ")";
+	//console.log("positive");
+	c.setAttribute("fill", color);
     }
     else {
-	c.setAttribute("fill", "red");
+	var color = "rgba(255,0,0," + (Math.abs(circle.q)/10) + ")";
+	c.setAttribute("fill", color);
     }
     c.setAttribute("stroke","black");
     svg.appendChild(c);
@@ -133,8 +157,8 @@ var action = function(e) {
 var updateCharge = function(e) {
     submit.disabled = true;
     input = textbox.value;
-    console.log("updating");
-    console.log(input);
+    //console.log("updating");
+    //console.log(input);
     if (input >= -100 && input <= 100 && input != "") {
 	submit.disabled = false;
 	svg.removeChild(lastCircleDrawn);
@@ -148,11 +172,9 @@ var assignCharge = function(e) {
     newCircleAllowed = true;
     lastCircleData = null;
     lastCircleDrawn = null;
+    textbox.value = 50;
     textbox.style.visibility = "hidden";
     submit.style.visibility = "hidden";
-    
-    
-    
 }
     
 svg.addEventListener("click", newCircle);
